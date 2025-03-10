9a7bb0b014143e419cb450d069ec89b1f1267de7
lynx212427 <135720642+lynx212427@users.noreply.github.com>
2025-03-10 06:14:59 +0000

    fix save note

--- OJT/save_note.php
+++ OJT/save_note.php
@@ -1,10 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+ 
   $date = $_POST['date'];
   $note = $_POST['note'];
 
+ 
   if ($date && $note) {
-    // Save to a file (or database)
     file_put_contents("notes.txt", "$date: $note\n", FILE_APPEND);
     echo json_encode(['status' => 'success']);
   } else {
